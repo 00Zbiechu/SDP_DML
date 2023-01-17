3436c1ee2ac094e35270751156051445fb9e5bb9
Windows <00Zbiewski@gmail.com>
2023-01-17 17:07:20 +0100

    Dodanie metod DML

---
+++ src/main/java/DBControll.java
@@ -0,0 +1,97 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBControll {
+
+     private PreparedStatement preparedStatement;
+     private ResultSet resultSet;
+
+
+    public void selectAccountRowFromAccountTableWhereSurnameAsParam(Connection connection, String surname){
+
+        final String QUERY = "SELECT * FROM account WHERE last_name=?";
+
+        try{
+
+            //SELECT 1
+            preparedStatement = connection.prepareStatement(QUERY);
+            preparedStatement.setString(1,surname);
+            resultSet = preparedStatement.executeQuery();
+
+            while(resultSet.next()){
+
+                System.out.println("Dane uzytkownika: ");
+                System.out.println("Imię:"+" "+resultSet.getString("first_name"));
+                System.out.println("Nazwisko:"+" "+resultSet.getString("last_name"));
+                System.out.println("Email:"+" "+resultSet.getString("email"));
+                System.out.println("Hasło:"+" "+resultSet.getString("password"));
+
+            }
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+    public void updateAccountWhereSurnameAsParam(Connection connection,String oldSurname, String newSurname){
+
+        final String UPDATE = "UPDATE account set last_name=? WHERE last_name=?";
+
+        try{
+
+            //UPDATE
+            preparedStatement = connection.prepareStatement(UPDATE);
+            preparedStatement.setString(1,newSurname);
+            preparedStatement.setString(2,oldSurname);
+            preparedStatement.executeUpdate();
+
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+    public void insertDataIntoAccountTable(Connection connection,String name,String surname,String email, String password){
+
+        final String INSERT = "INSERT INTO account(first_name, last_name, email, password) VALUES(?,?,?,?)";
+
+
+        try{
+
+            //INSERT
+            preparedStatement = connection.prepareStatement(INSERT);
+            preparedStatement.setString(1,name);
+            preparedStatement.setString(2,surname);
+            preparedStatement.setString(3,email);
+            preparedStatement.setString(4,password);
+            preparedStatement.executeUpdate();
+
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }
+
+
+
+    }
+
+    public void closeConnection(){
+
+        try{preparedStatement.close();}catch(SQLException e){e.printStackTrace();}
+        try{resultSet.close();}catch(SQLException e){e.printStackTrace();}
+
+    }
+
+}
